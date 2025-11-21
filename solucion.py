022f56dd06c8fd3d9688e32352ea70bdcd1e3595
Miriam <repdra2116@gmail.com>
2025-11-21 14:27:44 -0700

    Correct indentation in triangle printing logic
    Fixed indentation for triangle printing loops.

--- solucion.py
+++ solucion.py
@@ -5,9 +5,9 @@ def triangulo_simetrico(m: int, s: str) -> str:
         return
           
     #Parte 1: triangulo creciente
-    for i in range (1, M + 1):
-         print(s * i)
+     for i in range (1, M + 1):
+          print(s * i)
     
     #Parte 2: triangulo decreciente
-    for i in range (m -1, 0 -1):
-         print(s * i)
+     for i in range (m -1, 0 -1):
+          print(s * i)
