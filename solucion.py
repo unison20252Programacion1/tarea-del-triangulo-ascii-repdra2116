ff2e240bfd2249521a78cb7aedc9957dc333403c
Miriam <repdra2116@gmail.com>
2025-11-21 14:59:48 -0700

    Add TODOs for validation and ASCII clock logic
    Added placeholders for validation and ASCII logic.

--- solucion.py
+++ solucion.py
@@ -5,12 +5,3 @@ def reloj_arena(m: int, s: str) -> str:
     
     # TODO: implementar la lógica para generar el reloj de arena en ASCII
     pass
-
-          
-    #Parte 1: triangulo creciente
-     for i in range (1, m + 1):
-          print(s * i)
-    
-    #Parte 2: triangulo decreciente
-     for i in range (m - 1, 0, -1):
-          print(s * i)
